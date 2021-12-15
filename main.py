772ecccc1cc3ad8c9dc16310ecbcc2091a85f1a4
Marina Villaschi <78309614+marinavillaschi@users.noreply.github.com>
2021-12-15 16:15:20 -0300

    teste horario

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def on_ready():
     scheduler = AsyncIOScheduler()
 
     # horário com 3h a mais para rodar no heroku (timezone UTC)
-    scheduler.add_job(job_bater_ponto, CronTrigger(day_of_week = "MON-FRI", hour="22", minute="10", second="0")) 
+    scheduler.add_job(job_bater_ponto, CronTrigger(day_of_week = "MON-FRI", hour="19", minute="18", second="0")) 
     scheduler.start()
 
 
