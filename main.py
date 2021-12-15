1a2e64d34de1aa5e0ac4e9ec8851ff564700244b
Marina Villaschi <78309614+marinavillaschi@users.noreply.github.com>
2021-12-15 18:27:46 -0300

    agora

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def on_ready():
     scheduler = AsyncIOScheduler()
 
     # horário com 3h a mais para rodar no heroku (timezone UTC)
-    scheduler.add_job(job_bater_ponto_1, CronTrigger(hour="1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23", minute="20, 22, 24, 27, 28, 30", second="0")) 
+    scheduler.add_job(job_bater_ponto_1, CronTrigger(hour="21", minute="28, 30", second="0")) 
     scheduler.start()
 
 
