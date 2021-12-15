85d0d2e9312e0d34298b991e16517f70c0263004
Marina Villaschi <78309614+marinavillaschi@users.noreply.github.com>
2021-12-15 17:30:37 -0300

    teste final parte 1

--- main.py
+++ main.py
@@ -16,7 +16,12 @@ async def on_ready():
     scheduler = AsyncIOScheduler()
 
     # horário com 3h a mais para rodar no heroku (timezone UTC)
-    scheduler.add_job(job_bater_ponto, CronTrigger(day_of_week = "MON-FRI", hour="23", minute="28", second="0")) 
+    scheduler.add_job(job_bater_ponto_1, CronTrigger(day_of_week = "MON-FRI", hour="01", minute="33", second="0")) 
+    scheduler.add_job(job_bater_ponto_2, CronTrigger(day_of_week = "MON-FRI", hour="02", minute="33", second="0")) 
+    scheduler.add_job(job_bater_ponto_3, CronTrigger(day_of_week = "MON-FRI", hour="03", minute="33", second="0")) 
+    scheduler.add_job(job_bater_ponto_4, CronTrigger(day_of_week = "MON-FRI", hour="04", minute="33", second="0")) 
+    scheduler.add_job(job_bater_ponto_5, CronTrigger(day_of_week = "MON-FRI", hour="05", minute="33", second="0")) 
+    scheduler.add_job(job_bater_ponto_6, CronTrigger(day_of_week = "MON-FRI", hour="06", minute="33", second="0")) 
     scheduler.start()
 
 
@@ -35,10 +40,35 @@ async def on_message(message):
         await channel.send("bom dia " + mention)
 
 
-async def job_bater_ponto():
+async def job_bater_ponto_1():
     await client.wait_until_ready()
     c = client.get_channel(918949634259431469)
-    await c.send("lembrar de bater ponto!!")
+    await c.send("teste 1")
+
+async def job_bater_ponto_2():
+    await client.wait_until_ready()
+    c = client.get_channel(918949634259431469)
+    await c.send("teste 2")
+
+async def job_bater_ponto_3():
+    await client.wait_until_ready()
+    c = client.get_channel(918949634259431469)
+    await c.send("teste 3")
+
+async def job_bater_ponto_4():
+    await client.wait_until_ready()
+    c = client.get_channel(918949634259431469)
+    await c.send("teste 4")
+
+async def job_bater_ponto_5():
+    await client.wait_until_ready()
+    c = client.get_channel(918949634259431469)
+    await c.send("teste 5")
+
+async def job_bater_ponto_6():
+    await client.wait_until_ready()
+    c = client.get_channel(918949634259431469)
+    await c.send("teste 6")
 
 
 TOKEN = config("token")
