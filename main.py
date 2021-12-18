019fd66931d55d8b60d7a3874bd5d8bbf74f7c8b
Marina Villaschi <78309614+marinavillaschi@users.noreply.github.com>
2021-12-18 10:39:55 -0300

    ajustes finais

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import discord
 from decouple import config
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
-# from discord_slash import SlashCommand, SlashContext
-
 
 intents = discord.Intents.default()
 intents.members = True 
@@ -13,19 +11,16 @@ client = discord.Client(intents = intents)
 async def on_ready():
     print("o bot ta on")
 
-    scheduler = AsyncIOScheduler()
-
     # horário com 3h a mais para rodar no heroku (timezone UTC)
-    scheduler.add_job(job_bater_ponto_1, CronTrigger(hour="12, 15, 16, 21", minute="15", second="0")) 
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(job_bater_ponto, CronTrigger(day_of_week="MON-FRI", hour="12, 15, 16, 21")) 
     scheduler.start()
 
 
-# slash = SlashCommand(client)
-
-# @slash.slash(name="test")
-# async def test(ctx: SlashContext):
-#     embed = discord.Embed(title="Embed Test")
-#     await ctx.send(embed=embed)
+async def job_bater_ponto():
+    await client.wait_until_ready()
+    c = client.get_channel(918949634259431469)
+    await c.send("Bater ponto!")
 
 
 @client.event
@@ -38,10 +33,6 @@ async def on_message(message):
     if(message.author.bot):
         return
 
-    # responder ao oi
-    if(content == "oi" + mention or content == "ola" or content == "olá"):
-        await channel.send("Tenha um ótimo dia " + mention)
-
     # responder ao bom dia
     if(content == "bom dia" or content == "dia"):
         await channel.send("Tenha um ótimo dia " + mention)
@@ -50,11 +41,6 @@ async def on_message(message):
     if(content == "good morning" or content == "morning"):
         await channel.send("Have a great day " + mention)
 
-
-async def job_bater_ponto_1():
-    await client.wait_until_ready()
-    c = client.get_channel(918949634259431469)
-    await c.send("Bater ponto!")
 
 
 TOKEN = config("token")
