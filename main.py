9fa5e7ddc1a8b2ce68878c1d4bcf0038f9309ab0
Marina Villaschi <78309614+marinavillaschi@users.noreply.github.com>
2021-12-20 12:07:16 -0300

    included delete message after 1h

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def on_ready():
 async def job_bater_ponto():
     await bot.wait_until_ready()
     c = bot.get_channel(918949634259431469)
-    await c.send("Bater ponto!")
+    await c.send("Bater ponto!", delete_after = 3600)
 
 
 @bot.event
@@ -45,51 +45,6 @@ async def on_message(message):
     # responder ao morning
     if(content == "good morning" or content == "morning"):
         await channel.send("Have a great day " + mention)
-
-
-@bot.command()
-async def info(ctx):
-    """
-    /info
-    """
-    embed = discord.Embed(
-        title="Olá! Eu sou o novo estagiário da Qesh!",
-        description="Aqui vai uma lista das coisas que posso fazer para você:",
-        color=discord.Colour.blue()
-        )
-
-    embed.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)
-
-    embed.add_field(
-        name='/calc   :abacus:',
-        value='Eu faço operações matemáticas simples.\n',
-        inline=False
-    )
-
-    embed.add_field(
-        name='/test   :test_tube:',
-        value='Eu só repito o argumento, é uma função de teste.\n',
-        inline=False
-    )
-
-    embed.add_field(
-        name=':alarm_clock: Lembrete para bater o ponto',
-        value='Eu passo pra te lembrar de bater o ponto!',
-        inline=False
-    )
-
-    await ctx.send(embed=embed)
-
-@bot.command(name="calc")
-async def calculate_expression(ctx, expression):
-    expression = "".join(expression)
-    response = eval(expression)
-    await ctx.send("Resposta = " + str(response))
-
-@bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
-
 
 
 TOKEN = config("token")
