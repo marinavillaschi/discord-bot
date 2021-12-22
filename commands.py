790df8291e8d502a714eaff822df7cf775fcb87a
Marina Villaschi <78309614+marinavillaschi@users.noreply.github.com>
2021-12-22 16:27:45 -0300

    last update

--- commands.py
+++ commands.py
@@ -24,12 +24,6 @@ async def info(ctx):
     )
 
     embed.add_field(
-        name='/test   :test_tube:',
-        value='Eu só repito o argumento, é uma função de teste.\n',
-        inline=False
-    )
-
-    embed.add_field(
         name=':alarm_clock: Lembrete para bater o ponto',
         value='Eu passo pra te lembrar de bater o ponto!',
         inline=False
@@ -42,14 +36,6 @@ async def calculate_expression(ctx, *expression):
     expression = ' '.join(expression)
     response = eval(expression)
     await ctx.send("Resposta = " + str(response))
-
-
-# import disnake
-# from disnake.ext import commands
-
-# @bot.slash_command(description="Multiplies the number by 7")
-# async def multiply(inter, number: int):
-#     await inter.response.send_message(number * 7)
 
 
 @bot.command()
