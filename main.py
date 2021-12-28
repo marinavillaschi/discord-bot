1a1cf9f6a77ef3a9ce1a21a190261586552ac90f
Marina Villaschi <78309614+marinavillaschi@users.noreply.github.com>
2021-12-28 15:25:06 -0300

    update job_bater_ponto

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 from decouple import config
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
+from randomPhrases import randomPhrases
+from datetime import datetime
 
 intents = discord.Intents.default()
 intents.members = True 
@@ -13,18 +15,25 @@ channel_id = config("channel_id")
 
 @bot.event
 async def on_ready():
-    print("o bot ta on")
+    print("----------------------------\no bot ta on\n----------------------------")
 
     # horário com 3h a mais para rodar no heroku (timezone UTC)
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(job_bater_ponto, CronTrigger(day_of_week="MON-FRI", hour="12, 15, 16, 21")) 
+    scheduler.add_job(job_bater_ponto, CronTrigger(day_of_week="MON-FRI", hour="12, 15, 16, 18, 21"))
     scheduler.start()
+    
 
 async def job_bater_ponto():
     await bot.wait_until_ready()
     c = bot.get_channel(int(channel_id))
-    await c.send("Bater ponto!", delete_after = 3600)
-
+    if datetime.now().hour == 9:
+        await c.send(randomPhrases(), delete_after = 3600)
+        await c.send(":white_check_mark:  Não esqueça de bater o ponto! ", delete_after = 3600)
+    elif datetime.now().hour == 18:
+        await c.send("Hora de bater ponto!\nBom descanso! :wave:  ", delete_after = 3600)
+    else:
+        await c.send("Bater ponto! ", delete_after = 3600)
+    
 
 @bot.event
 async def on_message(message):
@@ -44,9 +53,13 @@ async def on_message(message):
     if(content == "good morning" or content == "morning"):
         await channel.send("Have a great day " + mention)
 
+    # responder ao bonjour
+    if(content == "bonjour" or content == "salut"):
+        await channel.send("Salut " + mention)
+
+    # teste do ponto
     if(content == "ponto"):
         await job_bater_ponto()
-
 
 
 TOKEN = config("token")
--- randomPhrases.py
+++ randomPhrases.py
@@ -2,14 +2,13 @@ import json
 from random import randint
  
 def randomPhrases():
-    phrases = open('morningPhrases.json')
+    phrases = open('morningPhrases.json', encoding = 'utf8')
 
     data = json.load(phrases)
     selectedPhrase = randint(0, len(data))
-    print(selectedPhrase)
 
     for phrase in data:
         if phrase['id'] == selectedPhrase:
-            return phrase['quote']
+            return phrase['quote'] + '\n- ' + phrase['author']
 
 randomPhrases()
